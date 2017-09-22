3662bb70015541c08c124f6cdc91045096e01b6a
horovtom <horovtom@fel.cvut.cz>
2017-09-22 21:42:11 +0200

    Adding decoding of json to startup-parse.sh

--- Startup-sequence/startup-parse.sh
+++ Startup-sequence/startup-parse.sh
@@ -1,8 +1,8 @@
-#!/bin/bash -       
+#!/bin/bash -
 #title           :startup-parse.sh
 #description     :This script will parse config file and carry out it's orders.
 #author		     :horovtom
-#version         :0.1    
+#version         :0.1
 #usage		     :bash startup-parse.sh
 #notes           :
 #==============================================================================
@@ -25,31 +25,34 @@ function loadConfig() {
         myEcho "Config not found! Maybe you deleted /tmp/gpjp-config ?"
         exit -1
     fi
-
+    
     source /tmp/gpjp-config/gpjp-startup-cfg.sh
+}
 
+function downloadInstructions() {
     if [ ! -x /tmp/gpjp-config/localSettings.sh ] ; then
         myEcho "Could not find local config so passing empty name!"
     else
         source /tmp/gpjp-config/localSettings.sh
     fi
-
+    
     name="${name//' '/%20}"
-}
-
-function downloadInstructions() {
+    
     myToken=$( echo $( sudo dmidecode -t 4 | grep ID | sed 's/.*ID://;s/ //g' ) \
     | sha256sum | awk '{print $1}' )
-
+    
     myEcho "Name is: "$name
-
+    
     request=$serverAddress"/api.php?token="$myToken"&name="$name
     
     myEcho "My token is: $myToken"
     myEcho "Sending request: $request"
-
+    
     response=$(curl $request)
     myEcho "Response was: "$response
+
+    firstField=$(echo $response | python -c 'import sys, json; print json.load(sys.stdin)["success"]')
+    myEcho "First field: "$firstField
 }
 
 
