90738a57dd2a53397a6b22ce2719166e8112369c
horovtom <horovtom@fel.cvut.cz>
2017-09-22 16:26:36 +0200

    Starting structure of bootstrap script

--- bootstrap.sh
+++ bootstrap.sh
@@ -7,3 +7,21 @@
 #notes           :
 #==============================================================================
 
+#Download startup.sh from some server (GIT? Our server?) 
+#I assume it will be on git:
+sudo apt-get install git -y
+#Curl way:
+#startupRepository="https://codeload.github.com/keombre/gpjp-config/zip/master"
+#curl -L $startupRepository> /tmp/startupRepository.zip
+#Git way:
+startupRepository="git://github.com/keombre/gpjp-config.git"
+sudo rm -rf /tmp/gpjp-startup
+git clone $startupRepository /tmp/gpjp-startup
+
+if [ $? -ne 0 ]; then
+    echo "There was an error while cloning repository!"
+    exit -1
+fi
+
+sudo cp /tmp/gpjp-startup/startup-sequence/startup.sh /etc/init.d/gpjp-startup.sh
+sudo chmod 755 /etc/init.d/gpjp-startup.sh
