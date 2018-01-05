cdf8516902a7bbaec3c112cb82dfaf058a080634
keombre <keombre8@gmail.com>
2018-01-05 23:15:21 +0100

    hotfix

--- units/install.sh
+++ units/install.sh
@@ -1,6 +1,4 @@
 mkdir -p /opt/hades
-mv start.sh /opt/hades
-mv hades.service /lib/systemd/system
-systemctl install hades
-systemctl enable hades
-systemctl start hades
+cp start.sh /opt/hades
+cp hades.service /lib/systemd/system
+sudo systemctl enable --now hades
